5604767bdad74deee5e98a62b7a72241e2fe27ff
V-keerththanan <keerththananphy@gmail.com>
2023-07-03 08:12:45 +0530

    added delete contact func in python

--- ContactManagementSystemInPython/AddressBook.py
+++ ContactManagementSystemInPython/AddressBook.py
@@ -4,10 +4,21 @@ from Contact import Contact
 class AddressBook:
     def __init__(self):
         self.ContactList:list=[]
+        self.emailList:list=[]
 
     def createContact(self,name,email,phone):
         contact=Contact(name,email,phone)
+        self.emailList.append(email)
         self.ContactList.append(contact)
 
+    def deleteContact(self,email:str):
+       if email in self.emailList:
+           for contact in self.ContactList:
+               if email==contact.getEmail():
+                   self.ContactList.remove(contact)
+                   print("contact removed!....")
+                   break
+       else:
+           print("There is no contact with given email!...")
 
 
